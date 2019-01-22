ea659fd2cbb50bda26804ff1e26e43c5c1927698
Yuchen Li <ychnlgy@gmail.com>
2019-01-22 13:14:31 -0500

    Changed accuracy score

--- examples/odenet_mnist.py
+++ examples/odenet_mnist.py
@@ -227,7 +227,7 @@ def one_hot(x, K):
 
 
 def accuracy(model, dataset_loader):
-    total_correct = 0
+    total_correct = n = 0.0
     for x, y in dataset_loader:
         x = x.to(device)
         y = one_hot(np.array(y.numpy()), 10)
@@ -235,7 +235,8 @@ def accuracy(model, dataset_loader):
         target_class = np.argmax(y, axis=1)
         predicted_class = np.argmax(model(x).cpu().detach().numpy(), axis=1)
         total_correct += np.sum(predicted_class == target_class)
-    return total_correct / len(dataset_loader.dataset)
+        n += len(predicted_class)
+    return total_correct / n
 
 
 def count_parameters(model):
